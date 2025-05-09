1ca64a03b141246798fe06c5326b5e0b11939e02
alvarte23a <alvar.tingelof@elev.ga.ntig.se>
2025-05-09 19:31:40 +0200

    lektion 4 som jag inte commitade tills nu
    fixade så att man kan titta höger och vänster (sprite vänder sig)

--- spelprojekt1.rb
+++ spelprojekt1.rb
@@ -2,12 +2,15 @@ require 'ruby2d'
 
 set title: "spelprojekt1", width: 1200, height: 675
 background = Image.new("bakgrund.jpg", x: 0, y: 0)
+player_right = Image.new("MEWOW.v2.png", x:0, y:475, width:100, height: 100)
+player_left = Image.new("image.png",x:0, y:475, width:100, height: 100 )
+text = Sprite.new("soace.png", x: 500, y: 100, width: 400  , height:400)
 
-player = Image.new("MEWOW.v2.png", x:300, y:300, width:100, height: 100)
-player_flipped = Image.new("image.png",x:300, y:300, width:100, height: 100 )
+obstacle = Rectangle.new(color:'black',x: 1190, y:475, width:15, height:10)
 #current_player = player
 
-
+blink_timer = 0
+blink_speed = 30
 rectangle = Rectangle.new(
   color: 'green',
   x: 0,
@@ -17,8 +20,25 @@ rectangle = Rectangle.new(
   
 )
 
+def switch_to(player_to_show)
+  current_player.remove
+  player_to_show.x = current_player.x
+  player_to_show.y = current_player.y
+  current_player = player_to_show
+  current_player.add
+end
 
 # Håller koll på nedtryckta tangenter
+all_pressed_keys = {}
+
+on :key_down do |event|
+  all_pressed_keys[event.key] = true  # lägg till nyckeln med ett värde
+  puts all_pressed_keys.keys          # skriver ut alla tangenter som tryckts ner
+end
+
+
+
+
 pressed_keys = {}
 
 # När en tangent trycks ner
@@ -32,16 +52,42 @@ on :key_up do |event|
 end
 
 
-velocity_y = 0          # Hastighet i Y-led
-gravity = 0.5           # Gravitationseffekt
-jump_strength = -10     # Hur starkt man hoppar (negativt = uppåt)
+velocity_y = 0 
+gravity = 0.5
+jump_strength = -15
 on_ground = false  
 
+facing_left = false
+facing_right = false
+text_exist = false
 update do
-  puts player.x
-  puts player.y
+
+if all_pressed_keys.include?("space")
+blink_timer += 0
+text_exist = true
+else 
+  blink_timer += 1
+end
+if text_exist == true
+  text.remove
+end
+
+
+if (blink_timer / blink_speed) % 2 == 0
+  text.add if text_exist == true
+  text_exist = true
+else
+  text.remove
+  text_exist = false 
+end
+  puts player_right.x
+  puts player_right.y
   move_x = 0
   move_y = 0
+  move_x_obstacle = 0
+
+player_left.remove
+current_player = player_right 
   
   if pressed_keys["w"] && on_ground
     velocity_y = jump_strength
@@ -49,38 +95,58 @@ update do
   end  
   velocity_y += gravity
 
-  player.x += move_x
-  player.y += velocity_y
-  if player.y + player.height >= rectangle.y
-    player.y = rectangle.y - player.height
+  player_right.x += move_x
+  player_right.y += velocity_y
+  if player_right.y + player_right.height >= rectangle.y
+    player_right.y = rectangle.y - player_right.height
     velocity_y = 0
     on_ground = true
   else
     on_ground = false
   end
 
-  if pressed_keys["d"] && player.x !=1100
+  if pressed_keys["d"] && current_player.x < 1100
+    current_player.remove
+    player_right.x = current_player.x
+    player_right.y = current_player.y
+    current_player = player_right
+    current_player.add
     move_x += 5
-    x_pos = player.x
-    y_pos = player.y
-   player.remove
-   player_flipped.x = x_pos
-   player_flipped.y = y_pos
   end
-  if pressed_keys["a"] && player.x !=0
-    move_x -= 5 
+  if pressed_keys["d"]
+    facing_left = false
+    facing_right = true
   end
-  if pressed_keys.empty? && 476 > player.y && 474 > player.y
-    player.y += 5
-  elsif pressed_keys["a"] && pressed_keys.length == 1 && 476 > player.y && 474 > player.y
-    player.y += 5
-  elsif pressed_keys["d"] && pressed_keys.length == 1 && 476 > player.y && 474 > player.y
-    player.y += 5 
+  
+  if pressed_keys["a"] && current_player.x > 0
+    current_player.remove
+    player_left.x = current_player.x
+    player_left.y = current_player.y
+    current_player = player_left
+    current_player.add
+    move_x -= 5
+  end
+  if pressed_keys["a"]
+    facing_right = false
+    facing_left = true
   end
 
+  if  facing_left == true
+    player_left.x = current_player.x
+    player_left.y = current_player.y
+    player_left.add
+  end
 
-  player.x += move_x
-  player.y += move_y
+  if all_pressed_keys.include?("space")
+    move_x_obstacle = -5
+  end
+  if obstacle.x == 5
+    obstacle.x = 1190
+  end
+
+  obstacle.x += move_x_obstacle
+  player_right.x += move_x
+  player_right.y += move_y
 
 end
 
